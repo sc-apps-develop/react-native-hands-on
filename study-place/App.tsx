cd40e2a940d59123249ceb27641d4c8d7dece714
sena-sakata <sena.sakata1@jp.ricoh.com>
2024-03-12 15:04:16 +0900

    device情報取得

--- study-place/App.tsx
+++ study-place/App.tsx
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+// import { useState } from "react";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// import { getUniqueId } from "react-native-device-info";
 
 export default function App() {
+  // const [deviceId, setDeviceId] = useState<string>();
+  // getUniqueId().then((id) => {
+  //   setDeviceId(id);
+  // });
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Text>{deviceId}</Text> */}
       <StatusBar style="auto" />
     </View>
   );
